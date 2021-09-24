488caa93dd36bb8914bdcea317cd695e8ed06a0b
Charlie Wiseman <charles.wiseman@bc.edu>
2021-09-24 13:27:31 -0400

    adding simple UDP examples

---
+++ csci3363/UDPecho1/EchoClient.java
@@ -0,0 +1,53 @@
+package csci3363.UDPecho1;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+public class EchoClient {
+
+    public static void main(String[] args) {
+        try (
+            /* open a socket on any unused UDP port */
+            var socket = new DatagramSocket();
+        ) {
+            /* set a message */
+            var message = "Hello!";
+
+            /* put the message into a buffer */
+            var out_buf = message.getBytes();
+
+            /* get the localhost address, set the destination port */
+            var dest_ip = InetAddress.getLocalHost();
+            var dest_port = 3363;
+
+            /* put the buffer into a packet, with destination address and port */
+            var out_packet = new DatagramPacket(out_buf, out_buf.length, dest_ip, dest_port);
+
+            /* send the message to the server */
+            socket.send(out_packet);
+
+            /* need a buffer to hold the response */
+            var in_buf = new byte[1600];
+
+            /* need a packet to hold the response */
+            var in_packet = new DatagramPacket(in_buf, in_buf.length);
+
+            /* read a packet from the socket */
+            socket.receive(in_packet);
+
+            /* convert the byte array to a string */
+            var in_msg = new String(in_buf, StandardCharsets.UTF_8);
+
+            /* print the server's response to the screen */
+            System.out.println(in_msg);
+
+        } catch(IOException e) {
+            /* Have to catch IOexceptions for most socket calls */
+            System.out.println("Network error!");
+        }
+    }
+}
+
