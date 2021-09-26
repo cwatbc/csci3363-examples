83c92abf7a553612ffa18618337a3b0dcdb7ad9d
Charlie Wiseman <charles.wiseman@bc.edu>
2021-09-26 14:19:37 -0400

    adding TCP echo 3

---
+++ csci3363/echo3/EchoClient.java
@@ -0,0 +1,39 @@
+package csci3363.echo3;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+
+    public static void main(String[] args) {
+        try (
+            /* open a connection localhost:3363 */
+            var socket = new Socket("localhost", 3363);
+
+            /* setup input and output streams for the socket */
+            var sock_in = new Scanner(socket.getInputStream());
+            var sock_out = new PrintWriter(socket.getOutputStream(), true);
+        ) {
+            /* get a message from the user */
+            var stdin = new Scanner(System.in);
+            System.out.print("Enter a message: ");
+            var message = stdin.nextLine();
+
+            /* send the message to the server */
+            sock_out.println(message);
+
+            /* read a single line response from the server */
+            var response = sock_in.nextLine();
+
+            /* print the server's response to the screen */
+            System.out.println(response);
+
+        } catch(Exception e) {
+            /* Have to catch exceptions for most socket calls */
+            System.out.println("Network error!");
+        }
+    }
+}
+
